a1094cabe6c30bfb82c5efcc8091439d7c4f3568
Michael Kubát <michchallll@gmail.com>
2025-02-12 21:23:18 +0100

    Byly upraveny výpisky pomocí funkce print a byla dokončena funkce main. Hra je nyní plně funční, také ohodnotí uživatelův výkon pomocí počtu pokusů a uplynulého času.

--- project_2.py
+++ project_2.py
@@ -41,27 +41,54 @@ def main():
         f"{'-' * 47}\n"
         f"I've generated a random 4 digit number for you.\n"
         f"Let's play a bulls and cows game.\n"
+        f"{'-' * 47}\n"
+        f"Enter a number:\n"
         f"{'-' * 47}"
     )
 
     secret_number = generate_secret_number()
-    print(secret_number)
     attempts = 0
     start_time = time.time()
     
     while True:
-        guess = input("Enter a number: ")
+        guess = input(">>> ")
         print("-" * 47)
         
         if not is_valid_guess(guess):
-            print("Invalid input! Enter a 4-digit number with unique digits.")
-            print("-" * 47)
+            print(
+                f"Invalid input! Enter a 4-digit number with unique digits.\n"
+                f"{'-' * 47}"
+            )
             continue
 
         attempts += 1
         bulls, cows = evaluate_guess(secret_number, guess)
 
-        print(bulls, cows)
+        if bulls == 4:
+            end_time = time.time()
+            duration = round(end_time - start_time, 2)
+            print(
+                f"Correct, you've guessed the right number\nin {attempts} guesses!\n"
+                f"Time taken: {duration} seconds\n"
+                f"{'-' * 47}"
+            )
+            
+            if attempts <= 5:
+                print("Outstanding performance!!! 🔥")
+            elif attempts <= 10:
+                print("Amazing performance! 🎉")
+            elif attempts <= 15:
+                print("Average performance. 👍")
+            else:
+                print("Not so good, better luck next time. 😅")
+            break
+
+        bull_text = "bull" if bulls == 1 else "bulls"
+        cow_text = "cow" if cows == 1 else "cows"
+        print(
+            f"{bulls} {bull_text}, {cows} {cow_text}\n"
+            f"{'-' * 47}"
+        )
 
 if __name__ == "__main__":
     main()
